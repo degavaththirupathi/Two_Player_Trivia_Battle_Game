138ff4f96f130cd7d69035784a6b34cdedd8be09
Degavath Thirupathi <107706675+degavaththirupathi@users.noreply.github.com>
2025-03-12 12:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,11 +118,7 @@ function endGame() {
 }
 
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+  return array.sort(() => Math.random()-0.5)
 }
 
 // New function to handle "Next Category" button click
